f1dffaed97a22b21d66c198be3481d16abb2ae17
AnnaVashuk <158236981+AnnaVashuk@users.noreply.github.com>
2025-04-09 20:09:07 +0300

    Update __init__.py

--- tmflow/utils/solver/__init__.py
+++ tmflow/utils/solver/__init__.py
@@ -1,17 +1,7 @@
 from .tm_solver import (
     TMSolver,
     run_tm_solver,
-    calc_numerical_solution,
-    get_next_point,
-    odeint,
-    torch_integrate
+    tmsolver_odeint as odeint
 )
 
-__all__ = [
-    'TMSolver',
-    'run_tm_solver',
-    'calc_numerical_solution',
-    'get_next_point',
-    'odeint',
-    'torch_integrate'
-]
+__all__ = ['TMSolver', 'run_tm_solver', 'odeint']
